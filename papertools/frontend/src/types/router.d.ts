b5304a1e9f03df7c61c94f8d15a90397e7664e54
eeeeeeex1 <2629620478@qq.com>
2025-07-05 08:55:10 +0800

    7.3

--- papertools/frontend/src/types/router.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/router.d.ts定义路由元数据类型
-import { RouteMeta } from 'vue-router'
-
-declare module 'vue-router' {
-  interface RouteMeta extends Record<string, any> {
-    requiresAuth?: boolean // 路由是否需要认证
-  }
-}
